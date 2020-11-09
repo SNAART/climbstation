dd036fb21cc4924c521fad693f2c604ef958a89f
Tran Nham <snaart1996@gmail.com>
2020-11-09 10:30:12 +0200

    init firebase_signin, added app logo

--- app/src/main/java/com/example/climbstation/MainActivity.kt
+++ app/src/main/java/com/example/climbstation/MainActivity.kt
@@ -3,17 +3,14 @@ package com.example.climbstation
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import com.example.climbstation.fragments.ClimbFragment
 import com.example.climbstation.fragments.CreateFragment
 import com.example.climbstation.fragments.SettingsFragment
 import com.example.climbstation.fragments.StatisticsFragment
-import com.google.zxing.integration.android.IntentIntegrator
-import com.journeyapps.barcodescanner.CaptureActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.login.*
-import kotlinx.android.synthetic.main.sign_in_form.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,14 +21,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
-        sign_in.setOnClickListener { goToSignIn() }
-    }
-
-    private fun goToSignIn() {
-        setContentView(R.layout.sign_in_form)
-        sign_in_2.setOnClickListener {
-            // TODO: login logic with firebase
-            startApp()
+        sign_in.setOnClickListener {
+            val intent = Intent (this, LoginActivity::class.java )
+            startActivity(intent)
+        }
+        sign_up.setOnClickListener {
+            val intent = Intent (this, SignupActivity::class.java)
+            startActivity(intent)
         }
     }
 
