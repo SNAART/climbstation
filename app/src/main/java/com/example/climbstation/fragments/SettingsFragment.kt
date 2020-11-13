10c782562f2d046c477ea78727e2fa6c5fa438ea
Rasmus Karling <rasmusjk@metropolia.fi>
2020-11-13 02:43:39 +0200

    shows current user in settings

--- app/src/main/java/com/example/climbstation/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/climbstation/fragments/SettingsFragment.kt
@@ -6,6 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.climbstation.R
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.fragment_settings.*
+import kotlinx.android.synthetic.main.fragment_settings.view.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -19,23 +22,27 @@ private const val ARG_PARAM2 = "param2"
  */
 class SettingsFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
+    private var username = FirebaseAuth.getInstance().currentUser?.email
     private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
+
             param2 = it.getString(ARG_PARAM2)
         }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+        savedInstanceState: Bundle?): View? {
+
+        val view = inflater!!.inflate(R.layout.fragment_settings, container, false)
+
+        view.tv_name.text = username
+        return view
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
     }
 
     companion object {
@@ -49,11 +56,12 @@ class SettingsFragment : Fragment() {
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance( param2: String) =
             SettingsFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                    putString(ARG_PARAM1, param2)
+                    tv_name.text = username
+
                 }
             }
     }
