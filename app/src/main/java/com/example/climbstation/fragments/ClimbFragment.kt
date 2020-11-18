1e64e564bdb2c78673ad792928da00dba405bc26
ienw <ienw@metropolia.fi>
2020-11-18 18:56:17 +0200

    created a list and showed the difficulty data

--- app/src/main/java/com/example/climbstation/fragments/ClimbFragment.kt
+++ app/src/main/java/com/example/climbstation/fragments/ClimbFragment.kt
@@ -1,11 +1,17 @@
 package com.example.climbstation.fragments
 
+import android.content.Context
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import com.example.climbstation.R
+import kotlinx.android.synthetic.main.fragment_climb.*
+import kotlinx.android.synthetic.main.fragment_climb.view.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -28,14 +34,51 @@ class ClimbFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
+        //renderList()
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_climb, container, false)
+        savedInstanceState: Bundle?): View? {
+
+        val view = inflater!!.inflate(R.layout.fragment_climb, container, false)
+        // Inflate the layout for this fragment view.mylist.adapter = ArrayAdapter(this.context, android.R.layout.simple_list_item_1,listOf("car", "plane"))
+        renderList(view)
+        return view
+    }
+
+
+    data class DifficultyData(val name: String, val level: Int)
+
+
+    private val listData: List<DifficultyData> = arrayListOf(
+        DifficultyData("Easy",1),
+        DifficultyData("Medium",2),
+        DifficultyData("Hard",3),
+    )
+
+
+    private var selection: DifficultyData = listData[0]
+
+
+    private fun renderList(view: View) {
+        val listItems = listData.map { SpannableString(it.name) }
+
+        val adapter = ArrayAdapter(
+            this.context,
+            android.R.layout.simple_list_item_1,
+            listItems
+        )
+        view.mylist.adapter = adapter
+        view.mylist.setOnItemClickListener { parent, view2, position, id ->
+            selection = listData[position]
+            view.difficulty.text = "Difficulty: ${selection.name}"
+            view.length.text = "Length: ${selection.level}"
+        }
+    }
+
+    private fun updateDetails(view: View) {
     }
 
     companion object {
