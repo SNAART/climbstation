c9cfda317620dedf03ee56c75acaf8da615625d3
ienw <ienw@metropolia.fi>
2020-12-02 18:31:34 +0200

    start and stop button logic with server connection

--- app/src/main/java/com/example/climbstation/fragments/ClimbFragment.kt
+++ app/src/main/java/com/example/climbstation/fragments/ClimbFragment.kt
@@ -6,14 +6,18 @@ import android.os.Bundle
 import android.text.Spannable
 import android.text.SpannableString
 import android.text.style.ForegroundColorSpan
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.example.climbstation.ConnectionInfo
 import com.example.climbstation.R
+import com.example.climbstation.retrofit.RestApiService
 import kotlinx.android.synthetic.main.fragment_climb.*
 import kotlinx.android.synthetic.main.fragment_climb.view.*
+import kotlin.concurrent.timer
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -29,6 +33,8 @@ class ClimbFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private var key: String? = null
+    private var started = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,13 +49,96 @@ class ClimbFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?): View? {
-
+        login()
         val view = inflater!!.inflate(R.layout.fragment_climb, container, false)
         // Inflate the layout for this fragment view.mylist.adapter = ArrayAdapter(this.context, android.R.layout.simple_list_item_1,listOf("car", "plane"))
         view.mode.text = "Mode: ${selectedMode}"
         view.mode.setOnClickListener{changeMode(view)}
         renderList(view)
+
+        view.start_button.setOnClickListener {
+            view.start_button.isEnabled = false
+            if (key != null) operate(view)
+        }
+
         return view
+    }
+
+    fun login() {
+        val apiService = RestApiService()
+        val userInfo = ConnectionInfo(
+            "2a",
+            1,
+            "admin",
+            "20110001",
+            "CLIMBSTATION",
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null
+        )
+
+        apiService.login(userInfo) {
+            Log.d("asd", "pääseekö tääne")
+            if (it != null) {
+                // it = newly added user parsed as response
+                // it?.id = newly added user ID
+                //  Log.d("http", it.toString())
+                // Log.d("http",it.clientKey)
+                key = it.clientKey.toString()
+                Log.d("asd",it.response)
+            } else {
+                Log.d("asd", "Error registering new user")
+            }
+        }
+    }
+
+    fun operate(view: View){
+        val apiService = RestApiService()
+        var operation: String = "start"
+        if (started == true){
+            operation = "stop"
+        }
+        val userInfo = ConnectionInfo(
+            "2c",
+            1,
+            null,
+            "20110001",
+            null,
+            null,
+            key,
+            null,
+            null,
+            null,
+            null,
+            operation,
+            null
+        )
+
+        apiService.operate(userInfo){
+            if (it != null) {
+                // it = newly added user parsed as response
+                // it?.id = newly added user ID
+                Log.d("asd", "start response ${it.response}")
+                if(it.response == "OK"){
+                    started = !started
+                    if (started) {
+                        //I should add a timer
+                        view.start_button.text = "Stop"
+                    } else {
+                        view.start_button.text = "Start"
+                    }
+                }
+            } else {
+                Log.d("asd", "Error getting info")
+            }
+            view.start_button.isEnabled = true
+        }
+
     }
 
     private val modeList: List<String> = arrayListOf("To next level", "Random", "Repeat", "Slow down")
